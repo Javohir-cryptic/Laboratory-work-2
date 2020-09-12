f2bac40cf48a319d9b9dd01e578f76e9bc8877f1
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-12 17:17:16 +0300

    Initial Commit

---
+++ Laboratory work №2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите ваше полное имя: ";
+    std::string myName;
+    std::getline(std::cin, myName);
+
+    std::cout << "Введите свой возраст: ";
+    int myAge;
+    std::cin >> myAge;
+
+    int letters = myName.length(); 
+    double agePerLetter = static_cast<double>(myAge) / letters; // используем оператор static_cast, чтобы изменить тип переменной myAge на double, дабы сохранить дробную часть при целочисленном делении
+    std::cout << "Вы прожили " << agePerLetter << "  года на каждую букву вашего имени.\n";
+
+    return 0;
+}
